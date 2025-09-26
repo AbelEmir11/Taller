89782ff235cf63248d9cfa2c269e5413de193a52
Abel12440 <g1911032@gmail.com>
2025-09-26 12:39:11 -0300

    quito el gunicorn por que generaba que se cuelgue el deploy en build, el gunicorn se ejecutara desde el yaml

--- render_build.sh
+++ render_build.sh
@@ -12,5 +12,3 @@ pip install -r requirements.txt
 # Ejecutar migraciones en la DB de Render
 flask db upgrade
 
-# Arrancar la app con gunicorn (como está configurado en render.yaml)
-gunicorn wsgi --chdir ./src/
